d4a709f1dfdd80bc229a303430ff5643a8dd5a68
yasindegerV <yasin.deger@virgosol.com>
2022-11-16 17:59:31 +0300

    last version

---
+++ Test Suites/testSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2c0a7d7e-b14b-4c5d-a447-82074130760d</testSuiteGuid>
+</TestSuiteEntity>
